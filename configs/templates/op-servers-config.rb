e52257d1789ee195bd2b462f09747e90924427d7
Mihail Sirbu <lenesu86@gmail.com>
2020-07-06 20:07:39 +0300

    fixed error

--- configs/templates/op-servers-config.rb
+++ configs/templates/op-servers-config.rb
@@ -4,7 +4,7 @@ class OpServers < OpBase
     stepbet_dev_old:  'ec2-54-202-14-77.us-west-2.compute.amazonaws.com',
     dietbet_dev_old:  'ec2-54-202-14-77.us-west-2.compute.amazonaws.com',
     stepbet_dev:  'ec2-35-161-73-241.us-west-2.compute.amazonaws.com',
-    stepbet_dev:  'ec2-35-161-73-241.us-west-2.compute.amazonaws.com',
+    dietbet_dev:  'ec2-35-161-73-241.us-west-2.compute.amazonaws.com',
   }
   SERVERS = {
     stepbet_dev:  {
