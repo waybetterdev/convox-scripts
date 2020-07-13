002e6f71b1e3603b211899ae945f8835af8611e6
Mihail Sirbu <lenesu86@gmail.com>
2020-07-13 13:12:15 +0300

    updated deploy configs

--- configs/templates/op-servers-config.rb
+++ configs/templates/op-servers-config.rb
@@ -1,25 +1,39 @@
 
 class OpServers < OpBase
   HOSTNAMES = {
-    stepbet_dev_old:  'ec2-54-202-14-77.us-west-2.compute.amazonaws.com',
-    dietbet_dev_old:  'ec2-54-202-14-77.us-west-2.compute.amazonaws.com',
-    stepbet_dev:  'ec2-35-161-73-241.us-west-2.compute.amazonaws.com',
-    dietbet_dev:  'ec2-35-161-73-241.us-west-2.compute.amazonaws.com',
+    stepbet_dev:  'dev.stepbet.com', # 'ec2-35-161-73-241.us-west-2.compute.amazonaws.com',
+    dietbet_dev:  'dev.dietbet.com', # 'ec2-35-161-73-241.us-west-2.compute.amazonaws.com',
+    dietbet_imageserver_dev: 'images.dev.dietbet.com', #'ec2-35-161-73-241.us-west-2.compute.amazonaws.com',
+    stepbet_imageserver_dev:  'images.dev.stepbet.com', # 'ec2-35-161-73-241.us-west-2.compute.amazonaws.com',
   }
   SERVERS = {
     stepbet_dev:  {
       user:  'centos', 
-      key:  '~/Work/deploy/keys/dietbet-dev-aws/dietbet-dev-aws.pem',
+      key:  '~/Work/deploy/keys/staging/staging.pem',
       hostnames:  [:stepbet_dev],
       src:  '/var/www/stepbet_dev', 
       dst:  '/var/www/dev.stepbet',
     },
+    stepbet_imageserver_dev:  {
+      user:  'centos', 
+      key:  '~/Work/deploy/keys/staging/staging.pem',
+      hostnames:  [:stepbet_imageserver_dev],
+      src:  '/var/www/stepbet_imageserver_dev', 
+      dst:  '/var/www/dev.images.stepbet',
+    },
     dietbet_dev:  {
       user:  'centos', 
-      key:  '~/Work/deploy/keys/dietbet-dev-aws/dietbet-dev-aws.pem',
+      key:  '~/Work/deploy/keys/staging/staging.pem',
       hostnames:  [:dietbet_dev],
       src:  '/var/www/dietbet_dev', 
       dst:  '/var/www/dev.dietbet',
+    },
+    dietbet_imageserver_dev:  {
+      user:  'centos', 
+      key:  '~/Work/deploy/keys/staging/staging.pem',
+      hostnames:  [:dietbet_imageserver_dev],
+      src:  '/var/www/dietbet_imageserver_dev', 
+      dst:  '/var/www/dev.images.dietbet',
     },
   }
 end
