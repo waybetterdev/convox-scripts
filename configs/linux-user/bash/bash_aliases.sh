6878287e495c3dc1b6a2c93748df5ccd5ec24acd
Mihail Sirbu <lenesu86@gmail.com>
2020-07-20 12:53:21 +0300

    fixed typo

--- configs/linux-user/bash/bash_aliases.sh
+++ configs/linux-user/bash/bash_aliases.sh
@@ -49,7 +49,7 @@ alias cdstepbetprod='cd /var/www/stepbet_prod;lcd'
 alias notepad=sublime-text.subl
 
 
-alias reloadaliases="cp -f ~/Work/docs/configs/linux-user/bash/bash_aliases.sh ~/.bash_aliases && cp -f ~/Work/docs/configs/linux-user/bash/bash_profile.sh ~/.bash_profile && source ~/.bash_aliases && source ~/.bash_profile && echo 'reloaded aliases and profile from linux-user-configs folder'"
+alias reloadaliases="cp -f ~/Work/docs/configs/linux-user/bash/bash_aliases.sh ~/.bash_aliases && cp -f ~/Work/docs/configs/linux-user/bash/bash_profile.sh ~/.bash_profile && source ~/.bash_aliases && source ~/.bash_profile && echo 'reloaded aliases and profile from linux-user folder'"
 
 alias restartapache='sudo systemctl restart apache2.service'
 
