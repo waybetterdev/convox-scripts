7ab843d9caa5db745f7d1d4b5c4aa96ec8e93866
Mihail Sirbu <lenesu86@gmail.com>
2020-07-20 13:17:23 +0300

    fixed paths

--- scripts/boot/ubuntu-user-startup.sh
+++ scripts/boot/ubuntu-user-startup.sh
@@ -36,7 +36,7 @@ echo $(date) + "Fixing DNS: "  >> /home/dev/tmux-boot-logs/wbtmux.txt 2>&1
 convox registries add 247028141071.dkr.ecr.us-west-2.amazonaws.com AWS $(aws ecr get-login-password --region us-west-2 --profile prod) >> /home/dev/tmux-boot-logs/wbtmux.txt 2>&1
 
 echo $(date) + ": running wbtmux" >> /home/dev/tmux-boot-logs/wbtmux.txt 2>&1
-ruby /home/dev/Work/docs/configs/scripts/ruby/wbtmux -r mysql,user,auth,bill,falkor,graphql,admin-auth,admin-web,metric,notify,member,runbet,social -o dietbet,hub -w >> /home/dev/tmux-boot-logs/wbtmux.txt 2>&1
+ruby /home/dev/Work/docs/scripts/ruby/wbtmux -r mysql,user,auth,bill,falkor,graphql,admin-auth,admin-web,metric,notify,member,runbet,social -o dietbet,hub -w >> /home/dev/tmux-boot-logs/wbtmux.txt 2>&1
 
 #echo $(date) + ": opening tmux" >> /home/dev/tmux-boot-logs/wbtmux.txt 2>&1
 #tmux attach -t lconvox >> /home/dev/tmux-boot-logs/wbtmux.txt 2>&1
