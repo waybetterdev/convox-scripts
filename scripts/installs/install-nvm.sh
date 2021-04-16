342fcf58d32f7deb9dd38a4bd059282c6322b164
Mihail Sirbu <lenesu86@gmail.com>
2021-04-16 19:40:35 +0300

    updated install nvm script

--- scripts/installs/install-nvm.sh
+++ scripts/installs/install-nvm.sh
@@ -16,20 +16,20 @@ if ! [ -x "$(command -v nvm)" ]; then
 	sleep 10s
 
 
-  sudo apt-get update && apt-get install -y \
-    vim \
-    curl \
-    less \
-    mysql-client
+  sudo apt-get update && sudo apt-get install -y vim curl less mysql-client
 
   sudo apt install curl 
-  curl https://raw.githubusercontent.com/creationix/nvm/master/install.sh | bash 
+  curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
 
-  source ~/.profile
 
   export NVM_DIR="$HOME/.nvm"
   [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
   [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+
+  source ~/.bashrc
+  source ~/.profile
+
+  nvm list-remote
 
   nvm install node 
 else
