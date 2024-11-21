f983a93277f2355b00c15d9a545fef92bf46938d
Mihail Sirbu <mihai@waybetter.com>
2024-11-21 12:41:53 +0200

    updated self signed certificates

--- scripts/installs/install-apache.sh
+++ scripts/installs/install-apache.sh
@@ -12,7 +12,7 @@ trap 'echo "\"${last_command}\" command failed with exit code $?."' EXIT
 
 if ! [ -x "$(command -v apache2)" ]; then
 	echo "Installing apache2"
-	echo "Sleeping for 10 seconds. Click ctrl+C to abort script." 
+	echo "Sleeping for 10 seconds. Click ctrl+C to abort script."
 	sleep 10s
 
 	sudo apt install -y apache2
@@ -28,19 +28,15 @@ fi
 
 if ! test -f "/etc/ssl/certs/apache-office-selfsigned.crt"; then
 	echo "Copying certificates to apache"
-	echo "Sleeping for 10 seconds. Click ctrl+C to abort script." 
+	echo "Sleeping for 10 seconds. Click ctrl+C to abort script."
 	sleep 10s
-	
+
 	sudo cp -v ~/Work/docs/certs/apache/office-selfsigned/office-selfsigned.crt /etc/ssl/certs/apache-office-selfsigned.crt
 	sudo cp -v ~/Work/docs/certs/apache/office-selfsigned/office-selfsigned.key /etc/ssl/private/apache-office-selfsigned.key
 	sudo cp -v ~/Work/docs/certs/apache/waybetterdev-selfsigned/waybetterdev-selfsigned.crt /etc/ssl/certs/apache-waybetterdev-selfsigned.crt
 	sudo cp -v ~/Work/docs/certs/apache/waybetterdev-selfsigned/waybetterdev-selfsigned.key /etc/ssl/private/apache-waybetterdev-selfsigned.key
 	sudo cp -v ~/Work/docs/certs/apache/ninja-selfsigned/ninja-selfsigned.crt /etc/ssl/certs/apache-ninja-selfsigned.crt
 	sudo cp -v ~/Work/docs/certs/apache/ninja-selfsigned/ninja-selfsigned.key /etc/ssl/private/apache-ninja-selfsigned.key
-	sudo cp -v ~/Work/docs/certs/apache/dietbet-selfsigned/dietbet-selfsigned.crt /etc/ssl/certs/apache-dietbet-selfsigned.crt
-	sudo cp -v ~/Work/docs/certs/apache/dietbet-selfsigned/dietbet-selfsigned.key /etc/ssl/private/apache-dietbet-selfsigned.key
-	sudo cp -v ~/Work/docs/certs/apache/vbox-selfsigned/vbox-selfsigned.crt /etc/ssl/certs/apache-vbox-selfsigned.crt
-	sudo cp -v ~/Work/docs/certs/apache/vbox-selfsigned/vbox-selfsigned.key /etc/ssl/private/apache-vbox-selfsigned.key
 	sudo cp -v ~/Work/docs/certs/apache/local-selfsigned/local-selfsigned.crt /etc/ssl/certs/apache-local-selfsigned.crt
 	sudo cp -v ~/Work/docs/certs/apache/local-selfsigned/local-selfsigned.key /etc/ssl/private/apache-local-selfsigned.key
 else
@@ -49,18 +45,16 @@ fi
 
 if ! test -f "/usr/share/ca-certificates/waybetterdev-selfsigned-rootCA.crt"; then
 	echo "Installing root certificates"
-	echo "Sleeping for 10 seconds. Click ctrl+C to abort script." 
+	echo "Sleeping for 10 seconds. Click ctrl+C to abort script."
 	sleep 10s
-	
-	
+
+
 	sudo cp -v ~/Work/docs/certs/apache/waybetterdev-selfsigned/waybetterdev-selfsigned-rootCA.crt /usr/share/ca-certificates/waybetterdev-selfsigned-rootCA.crt
 	sudo cp -v ~/Work/docs/certs/apache/ninja-selfsigned/ninja-selfsigned-rootCA.crt /usr/share/ca-certificates/ninja-selfsigned-rootCA.crt
-	sudo cp -v ~/Work/docs/certs/apache/dietbet-selfsigned/dietbet-selfsigned-rootCA.crt /usr/share/ca-certificates/dietbet-selfsigned-rootCA.crt
 	sudo cp -v ~/Work/docs/certs/apache/office-selfsigned/office-selfsigned-rootCA.crt /usr/share/ca-certificates/office-selfsigned-rootCA.crt
 	sudo cp -v ~/Work/docs/certs/apache/local-selfsigned/local-selfsigned-rootCA.crt /usr/share/ca-certificates/local-selfsigned-rootCA.crt
 	sudo bash -c 'echo "waybetterdev-selfsigned-rootCA.crt" >> /etc/ca-certificates.conf'
 	sudo bash -c 'echo "ninja-selfsigned-rootCA.crt" >> /etc/ca-certificates.conf'
-	sudo bash -c 'echo "dietbet-selfsigned-rootCA.crt" >> /etc/ca-certificates.conf'
 	sudo bash -c 'echo "office-selfsigned-rootCA.crt" >> /etc/ca-certificates.conf'
 	sudo bash -c 'echo "local-selfsigned-rootCA.crt" >> /etc/ca-certificates.conf'
 	sudo update-ca-certificates
@@ -70,13 +64,14 @@ fi
 
 if ! [ -x "$(command -v php)" ]; then
 	echo "Installing php 7.4 and mysqli"
-	echo "Sleeping for 10 seconds. Click ctrl+C to abort script." 
+	echo "Sleeping for 10 seconds. Click ctrl+C to abort script."
 	sleep 10s
 
 	
-	sudo apt-get install -y libapache2-mod-php7.4 
-	sudo apt-get install -y php-mbstring php7.4-curl
-	sudo a2enmod php7.4 
+	sudo apt-get install -y libapache2-mod-php7.4
+	sudo apt-get install -y php7.4-mbstring php7.4-curl
+	sudo a2enmod php7.4
+  
 	sudo apt-get install -y php7.4-mysqli
 	sudo apt-get install -y php7.4-pgsql
 	sudo apt-get install php7.4-dom
@@ -88,7 +83,7 @@ fi
 
 if ! test -d "/var/www/wb-proxy"; then
 	echo "Installing https-proxy and phpmyadmin"
-	echo "Sleeping for 10 seconds. Click ctrl+C to abort script." 
+	echo "Sleeping for 10 seconds. Click ctrl+C to abort script."
 	sleep 10s
 
 	echo "Creating /var/www path"
@@ -105,7 +100,7 @@ fi
 
 if ! test -d "/var/www/phpmyadmin"; then
 	echo "Installing https-proxy and phpmyadmin"
-	echo "Sleeping for 10 seconds. Click ctrl+C to abort script." 
+	echo "Sleeping for 10 seconds. Click ctrl+C to abort script."
 	sleep 10s
 
 	echo "Installing phpmyadmin"
@@ -122,10 +117,10 @@ fi
 
 if ! test -f "/etc/apache2/sites-enabled/wb-proxy-ssl.conf"; then
 	echo "Installing apache conf files"
-	echo "Sleeping for 10 seconds. Click ctrl+C to abort script." 
+	echo "Sleeping for 10 seconds. Click ctrl+C to abort script."
 	sleep 10s
 
-	echo "Generating apache conf." 
+	echo "Generating apache conf."
 	~/Work/docs/scripts/installs/apache-conf/build-apache-conf-and-install.sh
 else
   echo 'Apache conf file is already installed. Skipping.'
